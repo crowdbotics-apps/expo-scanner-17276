73541eed81e811adb8cac206773677814530ec1b
Crowdbotics <team@crowdbotics.com>
2020-05-22 07:41:35 +0000

    Initial commit.

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.expo_scanner_17276.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/expo_scanner_17276/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for expo_scanner_17276 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expo_scanner_17276.settings")
+
+application = get_wsgi_application()
